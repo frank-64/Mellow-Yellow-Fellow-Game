99e31f270fc6c0750959cf622a8bd8b22d7e4af5
RnJhbmsEquals <fcminipad@gmail.com>
2021-04-23 21:42:42 +0100

    Level added to GameUI so player can see the current level. Preparation is done for me to start working on creating more levels with more ghosts, lower powerup time and so on.

---
+++ Assets/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject game = GameObject.Find("Game");
+        YellowFellowGame yellowFellowGame = game.GetComponent<YellowFellowGame>();
+        int level = yellowFellowGame.level;
+        GetComponent<Text>().text = "" + level;
+    }
+}
